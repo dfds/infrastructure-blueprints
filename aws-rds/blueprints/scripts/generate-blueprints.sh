1f3abf942f3c9ed6b0906d6e75c34b121c295703
samidbb <samdi@dfds.com>
2024-01-02 15:03:37 +0100

    test run

--- aws-rds/blueprints/scripts/generate-blueprints.sh
+++ aws-rds/blueprints/scripts/generate-blueprints.sh
@@ -1,49 +1,13 @@
 # This will load the scaffolding scripts from aws-modules-rds and generate the blueprints
 git clone --branch feature/scaffolding_tools https://github.com/dfds/aws-modules-rds.git
 
-cd aws-modules-rds
+cd aws-modules-rds/tools/scaffolding
 
+ls -a
 
-ll -a
-# ./generate-blueprints.sh
+./generate_blueprints.sh
 
+cp -a auto-generated/. ../../easycreate
 
-
-
-
-# curl -sL https://github.com/dfds/aws-modules-rds/archive/main.tar.gz | tar xz
-
-
-
-
-
-
-# https://github.com/dfds/aws-backup-observability/releases
-
-
-
-# curl -L https://github.com/dfds/aws-backup-observability/archive/refs/tags/1.29.13.zip -o aws-modules-rds.zip
-
-
-
-
-# curl -L https://github.com/dfds/aws-backup-observability/releases/download/v1.29.13/1.29.13.tar.xz /tmp
-
-
-
-
-
-
-
-
-curl -sL https://github.com/dfds/aws-backup-observability/archive/main.tar.gz -o temp.tar.gz | tar xz -f temp
-
-
-
-
-working:
-curl -sL https://github.com/dfds/aws-backup-observability/archive/main.tar.gz | tar xz
-
-
-
-mkdir your-dir && curl -sL https://github.com/dfds/aws-backup-observability/archive/main.tar.gz | tar -cf your-dir --strip-components=1
+# cd ../../..
+#rm -rf aws-modules-rds
