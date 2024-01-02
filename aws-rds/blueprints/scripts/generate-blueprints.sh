de0c9992268051673c239d407d33dbc5f867f372
samidbb <samdi@dfds.com>
2024-01-02 15:24:27 +0100

    script..

--- aws-rds/blueprints/scripts/generate-blueprints.sh
+++ aws-rds/blueprints/scripts/generate-blueprints.sh
@@ -1,15 +1,17 @@
 # This will load the scaffolding scripts from aws-modules-rds and generate the blueprints
 # git clone --branch feature/scaffolding_tools https://github.com/dfds/aws-modules-rds.git # TODO: Checkout repo or download zip of the latest release
-echo "Cloned aws-modules-rds"
-ls -a
-cd /aws-rds/blueprints/scripts/aws-modules-rds/tools/scaffolding
-echo "Running generate_blueprints.sh"
-ls -a
+echo "list files in /aws-rds/"
+ls /aws-rds/
 
-./generate_blueprints.sh
+echo "list files in /aws-rds/blueprints/"
+ls /aws-rds/blueprints/scripts/aws-modules-rds/tools/scaffolding
+
+echo "Running generate_blueprints.sh"
+./aws-rds/blueprints/scripts/aws-modules-rds/tools/scaffolding/generate_blueprints.sh
 echo "Finished running generate_blueprints.sh"
 ls -a
-cp -a auto-generated/. ../../easycreate
+cp -a /aws-rds/blueprints/scripts/aws-modules-rds/tools/scaffolding/auto-generated/. /aws-rds/blueprints/easycreate
 
+ls /aws-rds/blueprints/easycreate
 # cd ../../..
 #rm -rf aws-modules-rds
