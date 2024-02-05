ea07373e6f7ce5288d20b61650bd2e3330cedd2a
devex-sa <65592364+devex-sa@users.noreply.github.com>
2024-02-05 12:20:38 +0000

    Github Actions update blueprints for RDS module

--- aws-rds-postgresql/generic/terraform/module.tf
+++ aws-rds-postgresql/generic/terraform/module.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 
 module "db_instance" {
-  source = "git::https://github.com/dfds/terraform-aws-rds.git?ref=1.0.2"
+  source = "git::https://github.com/dfds/terraform-aws-rds.git?ref=1.1.0"
 
   #     Provide a cost centre for the resource.
   #     Valid Values: .
